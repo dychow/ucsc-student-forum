780d6dd35b9dd3511065e07ddc597dfc31828172
zhuang43 <zhuang43@ucsc.edu>
2018-01-22 16:47:46 -0800

    Add files via upload

---
+++ UCSCForum/UCSCForum/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  UCSCForum
+//
+//  Created by MacDouble on 1/20/18.
+//  Copyright © 2018 MacDouble. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

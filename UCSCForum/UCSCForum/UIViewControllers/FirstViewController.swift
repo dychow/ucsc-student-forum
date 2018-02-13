5085cae1f08176aa99e24418c80ee1df68ea2607
Daniel Chow <Daniel@eduroam-169-233-206-142.ucsc.edu>
2018-02-12 16:35:32 -0800

    created linked list to store database fields

--- UCSCForum/UCSCForum/UIViewControllers/FirstViewController.swift
+++ UCSCForum/UCSCForum/UIViewControllers/FirstViewController.swift
@@ -12,6 +12,134 @@ import Firebase
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    public class Node {
+        private var itemAddress: String
+        private var itemCategory: String
+        private var itemDelivery: Bool
+        private var itemDetail: String
+        private var itemName: String
+        weak var prev: Node? = nil
+        var next: Node? = nil
+        
+        public init() {
+            self.itemAddress = ""
+            self.itemCategory = ""
+            self.itemDelivery = false
+            self.itemDetail = ""
+            self.itemName = ""
+        }
+        
+        public var getAddress: String {
+            return self.itemAddress
+        }
+        
+        public var getCategory: String {
+            return self.itemCategory
+        }
+        
+        public var getDelivery: Bool {
+            return self.itemDelivery
+        }
+        
+        public var getDetail: String {
+            return self.itemDetail
+        }
+        
+        public var getName: String {
+            return self.itemName
+        }
+        
+        public func setAddress(address: String){
+            self.itemAddress = address
+        }
+        
+        public func setCategory(category: String){
+            self.itemCategory = category
+        }
+        
+        public func setDelivery(delivery: Bool){
+            self.itemDelivery = delivery
+        }
+        
+        public func setDetail(detail: String){
+            self.itemDetail = detail
+        }
+        
+        public func setName(name: String){
+            self.itemName = name
+        }
+        
+    }
+    
+    public class LinkedList {
+        fileprivate var head: Node?
+        private var tail: Node?
+        private var cur: Node?
+        private var count: Int? = 0
+        
+        public var isEmpty: Bool {
+            return head == nil
+        }
+        
+        public var first: Node? {
+            return head
+        }
+        
+        public var last: Node? {
+            return tail
+        }
+        
+        public var current: Node? {
+            return cur
+        }
+        
+        public var counter: Int? {
+            return count
+        }
+        
+        public func append(node: Node) {
+            count = count! + 1
+            if let tailNode = tail {
+                node.prev = tailNode
+                tailNode.next = node
+            }
+            else {
+                head = node
+            }
+            tail = node
+        }
+        
+        public func delete(node: Node) {
+            count = count! - 1
+            let prev = node.prev
+            let next = node.next
+            
+            if let prev = prev {
+                prev.next = next
+            } else {
+                head = next
+            }
+            next?.prev = prev
+            
+            node.prev = nil
+            node.next = nil
+        }
+        
+        public func printList() {
+            var iterator = first
+            while iterator != nil {
+                print(iterator!.getName)
+                print(iterator!.getAddress)
+                print(iterator!.getCategory)
+                print(iterator!.getDelivery)
+                print(iterator!.getDetail)
+                
+                iterator = iterator?.next
+            }
+        }
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -54,27 +182,31 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         ref.observeSingleEvent(of: .value, with: {
             (snapshot) in
-            
+            let list = LinkedList()
             for child in snapshot.children {
+                let node = Node()
                 for grandchild in (child as AnyObject).children {
                     let snap = grandchild as! DataSnapshot
                     let key = snap.key
                     let value = snap.value
                     switch key {
                     case "itemName":
-                        print("post name = \(value!)")
+                        node.setName(name: value! as! String)
                     case "itemAddress":
-                        print("address = \(value!)")
+                        node.setAddress(address: value! as! String)
                     case "itemCategory":
-                        print("category = \(value!)")
+                        node.setCategory(category: value! as! String)
                     case "itemDelivery":
-                        print("delivery = \(value!)")
+                        node.setDelivery(delivery: value! as! Bool)
                     default:
-                        print("detail = \(value!)")
+                        node.setDetail(detail: value! as! String)
                     }
                 }
+                list.append(node: node)
             }
+            list.printList()
         })
+        
 
          // ref.observeSingleEvent(of: .value, with: {
          //   (snapshot) in

3397b1536ddd5a413ec8f60218fab30358df7fe9
YaxiBabe <bond.zhang1995@gmail.com>
2018-03-11 17:44:17 -0700

    user info can be pulled from database

--- UCSCForum/UCSCForum/UIViewControllers/TableViewController.swift
+++ UCSCForum/UCSCForum/UIViewControllers/TableViewController.swift
@@ -35,12 +35,11 @@ class TableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         print("before")
-        checkLogin()
+        getUserData()
         //self.performSegue(withIdentifier: "loginSegue", sender: self)
         print("afer")
         
         tableview.reloadData()
-        
         
         
     }
@@ -84,36 +83,32 @@ class TableViewController: UITableViewController {
         
         if Auth.auth().currentUser != nil{
             let uid = Auth.auth().currentUser?.uid
-            var ref = Database.database().reference().child("users").child(uid!)
+            let ref = Database.database().reference().child("users").child(uid!)
             
             ref.observeSingleEvent(of: .value, with: { (snapshot) in
                 let value = snapshot.value as? NSDictionary
-                self.name = value!["name"] as! String
-                self.bio = value!["about"] as! String
-                self.profileImageUrl = value!["profileImageUrl"] as! String
+                self.name = value!["name"] as? String
+                self.bio = value!["about"] as? String
+                self.profileImageUrl = value!["profileImageUrl"] as? String
                 
+                print(self.name)
+                print(self.bio)
+                self.nameLabel.text = self.name
+                self.bioLabel.text = self.bio
                 
+                if let url = NSURL(string: self.profileImageUrl!) {
+                    if let data = NSData(contentsOf: url as URL) {
+                        self.profileImage.image = UIImage(data: data as Data)
+                    }
+                }
                 UserDefaults.standard.setValuesForKeys(["name": self.name])
                 UserDefaults.standard.setValuesForKeys(["bio": self.bio])
                 UserDefaults.standard.setValuesForKeys(["profileImageUrl": self.profileImageUrl])
                 
             })
             
-            
         }
 
-        name = UserDefaults.standard.value(forKey: "name") as! String
-        bio = UserDefaults.standard.value(forKey: "bio") as! String
-        profileImageUrl = UserDefaults.standard.value(forKey: "profileImageUrl") as! String
-        
-        nameLabel.text = name
-        bioLabel.text = bio
-        
-        if let url = NSURL(string: profileImageUrl!) {
-            if let data = NSData(contentsOf: url as URL) {
-                profileImage.image = UIImage(data: data as Data)
-            }
-        }
     }
 
     func checkLogin(){
@@ -135,68 +130,6 @@ class TableViewController: UITableViewController {
         
     }
     
-    // override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    //return 0
-    // }
-    
-    //  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    //     return 1
-    //  }
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
